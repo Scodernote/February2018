bf706ba4096897c3fdfd3b6f5f7232a00db61b43
Scodernote <ashaan.arun@gmail.com>
2018-02-17 14:13:08 +0530

    Add files via upload

---
+++ RandomStringGenerator.java
@@ -0,0 +1,11 @@
+public class RandomStringGenerator {
+	private static final String ALPHA_NUMERIC_STRING = "abcdefghijklmnopqrstuvwxyz";
+	public static String randomAlphaNumeric(int count) {
+	StringBuilder builder = new StringBuilder();
+	while (count-- != 0) {
+	int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
+	builder.append(ALPHA_NUMERIC_STRING.charAt(character));
+	}
+	return builder.toString();
+	}
+}
